ff09f596e0a53d6ed7753fd406116a2e19334548
rajiur-rahman <rajiur.raju103@gmail.com>
2020-04-21 01:27:38 +0600

    Updates

---
+++ precache-manifest.2fd892a2a1b85e2e79394128448be1f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80b44085036c5f725fbf488a6a10fb96",
+    "url": "/Covid-19-Tracking-Info-With-React/index.html"
+  },
+  {
+    "revision": "dd3f8e9adb2764a2d73d",
+    "url": "/Covid-19-Tracking-Info-With-React/static/css/main.f4c9f081.chunk.css"
+  },
+  {
+    "revision": "efa0ff3d8c97be0a2a78",
+    "url": "/Covid-19-Tracking-Info-With-React/static/js/2.2244ece3.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid-19-Tracking-Info-With-React/static/js/2.2244ece3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd3f8e9adb2764a2d73d",
+    "url": "/Covid-19-Tracking-Info-With-React/static/js/main.5f6eff9b.chunk.js"
+  },
+  {
+    "revision": "6600c7e6b5731aba49b9",
+    "url": "/Covid-19-Tracking-Info-With-React/static/js/runtime-main.13ee1946.js"
+  }
+]);
